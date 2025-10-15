733506f54079d7438f832be5c24cf5873763add2
youyacoder <yygnb@126.com>
2025-10-15 15:09:38 +0800

    feat: 添加路由守卫

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,19 +2,37 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { routes } from 'vue-router/auto-routes'
 import { setupLayouts } from 'virtual:generated-layouts'
 import type { App } from 'vue'
+import { useCoreStore } from '@/stores/modules/core.ts'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: setupLayouts(routes),
 })
 
-export const installRouter = (app: App) => {
-  app.use(router)
-}
-
 export const PATH_HOME = '/'
 export const PATH_LOGIN = '/login'
 export const PATH_REGISTER = '/register'
 export const PATH_MANAGE_INDEX = '/manage/list'
+
+const whiteList = [PATH_HOME, PATH_LOGIN, PATH_REGISTER]
+
+router.beforeEach((to) => {
+  const coreStore = useCoreStore()
+  const token = coreStore.token
+  if (token) {
+    if (to.path === PATH_LOGIN) {
+      return PATH_MANAGE_INDEX
+    }
+    return true
+  }
+  if (whiteList.includes(to.path)) {
+    return true
+  }
+  return PATH_LOGIN
+})
+
+export const installRouter = (app: App) => {
+  app.use(router)
+}
 
 export default router
